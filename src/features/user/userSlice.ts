382fe192b73d38ac1ad45901c5714a98f62c3350
Matt Keane <mattpkeane@gmail.com>
2022-04-06 15:44:53 -0500

    user registration works

--- src/features/user/userSlice.ts
+++ src/features/user/userSlice.ts
@@ -9,8 +9,15 @@ interface LogInPayload {
     password: string
 }
 
-export const logInUser = createAsyncThunk(
-    // thunk to handle user log ins
+interface RegisterPayload {
+    username: string,
+    email: string,
+    password: string,
+    verifyPassword?: string
+}
+
+// thunk to handle user log ins
+export const logInUser = createAsyncThunk(    
     'user/logIn',
     async (payload: LogInPayload, thunkAPI) => {
         const { REACT_APP_API_URL: baseUrl } = process.env
@@ -26,19 +33,47 @@ export const logInUser = createAsyncThunk(
             })
             if (logInResponse.status === 200) {
                 // fulfill on successful api request
-                console.log('status 200')
                 const logInJson = await logInResponse.json()
                 return logInJson.data
             } else if (logInResponse.status === 401) {
                 // reject with invalid message when credentials are invalid
-                console.log('status 401')
                 return thunkAPI.rejectWithValue('invalid')
             }
         } catch (err) {
-            // reject with error on erros
+            // reject with error on errors
             return thunkAPI.rejectWithValue('error')
         }
         
+    }
+)
+
+// thunk to handle user registration
+export const registerUser = createAsyncThunk(
+    'user/register',
+    async (payload: RegisterPayload, thunkAPI) => {
+        const { REACT_APP_API_URL: baseUrl } = process.env
+        const url = baseUrl + '/api/v1/users/register'
+        try {
+            const registerResponse = await fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(payload),
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            })
+            if (registerResponse.status === 201) {
+                // fulfill on succesful api request
+                const registerJson = await registerResponse.json()
+                return registerJson.data
+            } else if (registerResponse.status === 401) {
+                // reject with exists message if username is taken
+                return thunkAPI.rejectWithValue('exists')
+            }
+        } catch (err) {
+            // reject on error
+            return thunkAPI.rejectWithValue('error')
+        }
     }
 )
 
@@ -65,9 +100,24 @@ export const userSlice = createSlice({
             })
             .addCase(logInUser.rejected, (state, action) => {
                 // set rejection status on login rejection
-                console.log('Rejected reducer running')
                 if (action.payload === 'invalid') {
                     state.status = 'rejected'
+                } else {
+                    state.status = 'error'
+                }
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                // update user on successful registration
+                const { username, id, email } = action.payload
+                state.username = username
+                state.id = id
+                state.email = email
+                state.status = 'success'
+            })
+            .addCase(registerUser.rejected, (state, action) => {
+                // set rejection status on registration rejection
+                if (action.payload === 'exists') {
+                    state.status = 'exists'
                 } else {
                     state.status = 'error'
                 }
--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,8 +1,16 @@
 import { 
     useState,
+    useEffect,
     ChangeEvent,
     SyntheticEvent, 
 } from 'react'
+import { 
+    useDispatch,
+    useSelector, 
+} from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { registerUser } from '../features/user/userSlice'
+import { State } from '../types'
 
 export default function Register() {
     const initialFormState = {
@@ -13,6 +21,24 @@ export default function Register() {
     }
 
     const [formState, setFormState] = useState(initialFormState)
+    const [message, setMessage] = useState('')
+    const user = useSelector((state: State) => state.user)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (user.status === 'success') {
+            // redirect to home page on successful registration
+            navigate('/')
+        } else if (user.status === 'exists') {
+            // display message if username is already taken
+            setMessage('Username taken')
+        } else if (user.status === 'error') {
+            // display message if an error has occured
+            setMessage('Error registering')
+        }
+    }, [user.status, navigate])
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setFormState(currentFormState => ({
@@ -23,11 +49,22 @@ export default function Register() {
 
     const handleSubmit = (e: SyntheticEvent) => {
         e.preventDefault()
-        console.log(formState)
+        if (formState.password === formState.verifyPassword) {
+            // attempt registration if passwords match
+            dispatch(registerUser(formState))
+        } else {
+            // display message if passwords don't match
+            setMessage('Passwords must match.')
+        }
     }
 
     return (
         <main>
+            {
+                message
+                &&
+                <p>{ message }</p>
+            }
             <form onSubmit={ handleSubmit }>
                 <fieldset>
                     <label htmlFor="username">
