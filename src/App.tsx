1a619bbce279bbec1402c8978bc463a22e4a043e
Matt Keane <mattpkeane@gmail.com>
2022-04-19 13:09:49 -0500

    removed old code from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,11 @@
-import { 
-  useState,
-  ChangeEvent,
-  SyntheticEvent,
-} from 'react';
 import './App.css';
-import {
-  Outlet,
-  useNavigate
-} from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { State } from './types'
 import NavBar from './components/NavBar'
 
 function App() {
-  const [searchQuery, setSearchQuery] = useState('')
   const userState = useSelector((state: State) => state.user)
-  const navigate = useNavigate()
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(e.target.value)
-  }
-
-  const handleSubmit = (e: SyntheticEvent) => {
-    e.preventDefault()
-    navigate('/search?q=' + searchQuery)
-  }
 
   return (
     <div className="App">
